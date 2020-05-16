65189b5e3ec83ea456993dc909e01c1f1b0442f2
siyeon <sylee@megazone.com>
2020-05-16 16:18:06 +0900

    Apply Fullpage.js

--- hackathon-mask/src/components/mainPage.vue
+++ hackathon-mask/src/components/mainPage.vue
@@ -1,20 +1,65 @@
 <template>
     <div>
-        <p>hello!</p>
-        <photoUpload/>
+        <div>
+            <full-page ref="fullpage" :options="options" id="fullpage">
+                <div class="section">
+                    <p class="title">#WearingMaskChallenge</p>
+                    <photo-upload/>
+                </div>
+                <div class="section">
+                    <div class="section2-msg"><p>우리는 늘 이겨냈고, 이번에도 함께 이겨낼 것입니다.</p>
+                        <p>힘든 시기를 헤쳐 나가는 모든 분들을 응원합니다.</p><br>
+                        <span>AUSG JSY Team</span>
+                    </div>
+                </div>
+            </full-page>
+        </div>
     </div>
 </template>
 
 <script>
     import photoUpload from './photoUpload.vue';
+    import {reactive, toRefs} from "@vue/composition-api";
     export default {
         name: "mainPage",
         components: {
             photoUpload
+        },
+        setup() {
+            const state = reactive({
+                options: {
+                    menu: '#menu',
+                    anchors: ['page1', 'page2'],
+                    sectionsColor: ['#000000', '#000000']
+                },
+            })
+
+            return {
+                ...toRefs(state),
+            }
         }
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    .title {
+        color: orange;
+        font-weight: bold;
+        font-size: 5rem;
+        text-align: center;
+        margin-top: -10rem;
+        font-family: 'Lato', sans-serif;
+    }
+    .section2-msg {
+        color: orange;
+        text-align: center;
+        p {
+            font-size: 3rem;
+            font-family: 'Do Hyeon', sans-serif;
+        }
+        span {
+            font-size: 2rem;
+            font-family: 'Do Hyeon', sans-serif;
+        }
+    }
 </style>
--- hackathon-mask/src/main.js
+++ hackathon-mask/src/main.js
@@ -3,9 +3,11 @@ import App from './App'
 import router from './router'
 import Router from "vue-router";
 import VueCompositionApi from '@vue/composition-api';
+import VueFullPage from 'vue-fullpage.js'
 
 Vue.use(VueCompositionApi);
 Vue.use(Router)
+Vue.use(VueFullPage);
 
 new Vue({
   el:'#app',
