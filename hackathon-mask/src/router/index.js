7281d32a79b69cacd7553cde3f7439cbeb9b87a9
siyeon <sylee@megazone.com>
2020-05-16 13:36:00 +0900

    Add history mode

--- hackathon-mask/src/router/index.js
+++ hackathon-mask/src/router/index.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
 import Router from 'vue-router'
 import main from '../components/mainPage.vue'
 
 
 export default new Router({
+    mode: 'history',
     routes: [
         {
-            path:'/main',
+            path:'/',
             name:'mainPage',
             component: main
         }
